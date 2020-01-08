4226b63fa6a74345975c0785082127318be36097
Luis Laborda <luis.laborda@mckesson.com>
2020-01-08 08:37:24 -0600

    Project setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+//Core packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+// CSS Style
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
